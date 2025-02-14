66976f4f3e39096cf8e12dcb50a0338d818bc2d3
Rajat Pandey <rajatpandey7800@gmail.com>
2025-02-14 23:10:13 +0530

    LinearSearch

--- Array/Searching/LinearSearch.go
+++ Array/Searching/LinearSearch.go
@@ -3,14 +3,13 @@ package main
 import "fmt"
 
 func linearSearch(arr []int, target int) {
-    for _, value := range(arr) {
+    for _, value := range arr {
         if value == target {
             fmt.Println("Element found in an array")
-			return
+            return
         }
-	}
+    }
     fmt.Println("Element not found")
-
 }
 
 func linearSearchMain() {

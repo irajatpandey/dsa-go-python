40b0e3c913e21167a0a340491fb6be2f71bb6ee6
Rajat Pandey <rajatpandey7800@gmail.com>
2025-02-15 17:24:52 +0530

    Leetcode: 485. Max Consecutive Ones

--- Array/leetcode/easy/MaximumConsecutiveOnes.go
+++ Array/leetcode/easy/MaximumConsecutiveOnes.go
@@ -1,1 +1,22 @@
 package main
+
+import "fmt"
+
+func findMaxConsecutiveOnes(arr []int) int {
+	maxCount := 0
+	ans := 0
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == 1 {
+			maxCount++
+		} else {
+			ans = max(ans, maxCount)
+			maxCount = 0
+		}
+	}
+	ans = max(ans, maxCount)
+	return ans
+}
+func findMaxConsecutiveOnesMain() {
+	arr := []int{1, 1, 0, 1, 1, 1}
+	fmt.Println(findMaxConsecutiveOnes(arr)) // Output: 3
+}

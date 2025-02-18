40d6c96250667b7362d6128e80f096af3260225e
Rajat Pandey <rajatpandey7800@gmail.com>
2025-02-18 19:29:33 +0530

    Leetcode: 48. Rotate Image

---
+++ Array/leetcode/medium/RotateImage.py
@@ -0,0 +1,20 @@
+#https://leetcode.com/problems/rotate-image/
+def rotate(matrix):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(n):
+        matrix[i].reverse()
+
+# Example usage
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    rotate(matrix)
+    print("Rotated matrix:")
+    for row in matrix:
+        print(row)

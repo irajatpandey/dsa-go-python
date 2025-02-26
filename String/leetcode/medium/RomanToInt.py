7af1690e870cd2197eb02739e4781dd08f0ed11f
Rajat Pandey <rajatpandey7800@gmail.com>
2025-02-26 10:36:43 +0530

    Leetcode: 13. Roman to Integer

--- String/leetcode/medium/RomanToInt.py
+++ String/leetcode/medium/RomanToInt.py
@@ -15,7 +15,7 @@ def main():
 
 class Solution:
     def romanToInt(self, s):
-        roman_map = {'I' : 1, 'V': 5, 'X' : 10, 'L' : 50, 'C' : 100, 'M' : 1000}
+        roman_map = {'I' : 1, 'V': 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
         ans = 0
         previous = None
         for char in s:

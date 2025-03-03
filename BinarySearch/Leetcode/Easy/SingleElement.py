797ceaf114bab6e120d4a5d3c3c7df6ac994de5e
Rajat Pandey <rajatpandey7800@gmail.com>
2025-03-03 19:39:53 +0530

    Leetcode: 540. Single Element in a Sorted Array

--- BinarySearch/Leetcode/Easy/SingleElement.py
+++ BinarySearch/Leetcode/Easy/SingleElement.py
@@ -1,19 +1,22 @@
-#https://leetcode.com/problems/single-element-in-a-sorted-array/
-class Solution(object):
-    def singleNonDuplicate(self, nums):
-        start, end, ans = 0, len(nums) - 1, -1
-        while start < end:
-            mid = start + (end - start) // 2
-            if mid % 2 == 0:
-                if nums[mid] == nums[mid + 1]:
-                    start = mid + 2
-                else:
-                    end = mid
-            else:
-                if nums[mid] == nums[mid - 1]:
-                    start = mid + 1
-                else:
-                    end = mid
-        return nums[start]
+def singleNonDuplicate(self, nums):
+        n = len(nums)
+        if len(nums) == 1:
+            return nums[0]
+        if nums[0] != nums[1]:
+             return nums[0]
 
-         
+        if nums[n - 1] != nums[n - 2]:
+            return nums[n - 1]
+        
+        start, end = 0, n - 2
+        while start <= end:
+            mid = start + (end - start) // 2
+            if nums[mid] != nums[mid + 1] and nums[mid] != nums[mid - 1]:
+                return nums[mid]
+            
+            if mid % 2 == 1 and nums[mid - 1] == nums[mid] or mid % 2 == 0 and nums[mid] == nums[mid + 1]:
+                start = mid + 1
+            else:
+                end = mid - 1
+        
+        return -1
